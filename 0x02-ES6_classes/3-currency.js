33286d2756f1dbcae69ac9ac1cbea9a1a43d649a
hananabo999 <139076368+hananabo999@users.noreply.github.com>
2024-06-27 23:42:58 +0200

    Update 3-currency.js

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -5,33 +5,33 @@ export default class Currency {
     } else if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
     }
-  
+
     this._code = code;
     this._name = name;
   }
-  
+
   get name() {
     return this._name;
   }
-  
+
   get code() {
     return this._code;
   }
-  
+
   set name(name) {
     if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
     }
     this._name = name;
   }
-  
+
   set code(code) {
     if (typeof code !== 'string') {
       throw new TypeError('Code must be a string');
     }
     this._code = code;
   }
-  
+
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
