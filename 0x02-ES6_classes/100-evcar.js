8b6039dc252d39aa21f3e9de6317ff8028f9cf5c
hananabo999 <139076368+hananabo999@users.noreply.github.com>
2024-06-27 23:35:22 +0200

    Update 100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,23 +1,14 @@
 import Car from './10-car';
 
-export default class EVCar extends Car {
-  constructor(brand, motor, color, range) {
+class EVCar extends Car {
+  constructor(brand = '', motor = '', color = '', range = '') {
     super(brand, motor, color);
-    this.range = range;
+    this._range = range;
   }
 
-  get range() {
-    return this._range;
-  }
-
-  set range(value) {
-    this._range = value;
-  }
-
-  cloneCar() {
-    const Species = super.constructor[Symbol.species];
-
-    return new Species();
+  static get [Symbol.species]() {
+    return Car;
   }
 }
 
+export default EVCar;
