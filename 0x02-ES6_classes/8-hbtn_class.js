b167dd781b0f85fe6b2653ca6c2ebde07155e0f0
hananabo999 <139076368+hananabo999@users.noreply.github.com>
2024-06-27 23:33:30 +0200

    Update 8-hbtn_class.js

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,33 +1,41 @@
 export default class HolbertonClass {
   constructor(size, location) {
-    this.size = size;
-    this.location = location;
+    this._size = this.validateNumber(size, 'Size');
+    this._location = this.validateString(location, 'Location');
   }
 
+  // Validator for numbers
+  validateNumber(value, attributeName) {
+    if (typeof value !== 'number') {
+      throw new TypeError(`${attributeName} must be a number`);
+    }
+    return value;
+  }
+
+  // Validator for strings
+  validateString(value, attributeName) {
+    if (typeof value !== 'string') {
+      throw new TypeError(`${attributeName} must be a string`);
+    }
+    return value;
+  }
+
+  // Getters
   get size() {
     return this._size;
-  }
-
-  set size(value) {
-    this._size = value;
   }
 
   get location() {
     return this._location;
   }
 
-  set location(value) {
-    this._location = value;
+  // Override valueOf
+  valueOf() {
+    return this._size;
   }
 
-  [Symbol.toPrimitive](hint) {
-    if (hint === 'number') {
-      return this.size;
-    }
-    if (hint === 'string') {
-      return this.location;
-    }
-    return this;
+  // Override toString
+  toString() {
+    return this._location;
   }
 }
-
